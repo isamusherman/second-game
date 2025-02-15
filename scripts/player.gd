7179ab1ce22279304fd15ab212cb2f58b7540dd4
Isamu Sherman <51054422+uncannierlink@users.noreply.github.com>
2025-02-16 00:55:46 +0900

    Animated Player Death
    Now player can die and it plays animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,27 +4,44 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
+var is_alive: bool = true
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+func die():
+	is_alive = false
+	animated_sprite.play("death")
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if is_alive:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("move_left", "move_right")
+		
+			
+		#This Applies Movement
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+		move_and_slide()
+		animate(direction)
 	
+func animate(direction):	
 	#Flip Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-	
+
 	#Play Anmiations
 	if is_on_floor():
 		if direction == 0:
@@ -33,11 +50,3 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jumping")
-	
-	#This Applies Movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
