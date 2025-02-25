f373d709633c40498da981d6872ed45af79dbb84
Isamu Sherman <isamusherman@gmail.com>
2025-02-25 10:17:42 -0500

    Added character state machine outline
    Now character controller can have more complex logic

--- scenes/character_state_machine.gd
+++ scenes/character_state_machine.gd
@@ -2,11 +2,13 @@ extends Node
 
 class_name CharacterStateMachine
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var states : Array[State]
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	for child in get_children():
+		if(child is State):
+			states.append(child)
+		
+		else:
+			push_warning("Child " + child.name + " is not a State for CharacterStateMachine")
